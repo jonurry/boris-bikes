5bbc48e01c627c463f2c3af154cf0b864f433824
NicGiles <n.r.giles1@gmail.com>
2018-09-05 18:11:01 +0100

    Raise and fail tasks

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -9,11 +9,19 @@ attr_reader :bike
   end
 
   def release_bike
+  @available_bikes = @bike
+  unless @available_bikes > 1
+    raise "there are no bikes"
+    end
   @bike
   end
 
-  def dock_bike(bike)
-  @bike = bike
+  def dock_bike
+    if @available_bikes == 0
+      @bike = bike
+    else
+      return "This dock is full :("
+    end
   end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,13 +1,18 @@
 require 'DockingStation'
+require 'Bike'
 
 describe DockingStation do
   # it "should create a DockingStation" do
   #   expect (DockingStation.new).to be_an_instance_of(DockingStation)
   # end
 
-  it "should respond to a message" do
-    expect(subject).to respond_to :release_bike
+  it "it true" do
+    expect {raise "No bikes here" }.to raise_error
   end
+
+  #it "should respond to a message" do
+  #  expect(subject).to respond_to :release_bike
+  #end    -  different method of the below
 
   it { should respond_to (:release_bike) }
 
@@ -18,5 +23,8 @@ describe DockingStation do
   #it "dock a bike" do
     #expect(subject.dock_bike).to be_an_instance_of(Bike)
   #end
+
+
   it { should respond_to (:dock_bike) }
+
 end
